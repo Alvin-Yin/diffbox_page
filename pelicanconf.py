39c9e0d650db8e46aa74a6cdbc8e553e70eb9c90
saber <revontulet.yin@gmail.com>
2022-04-18 16:11:13 +0800

    document-relative URLs when developing

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,4 +31,4 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
